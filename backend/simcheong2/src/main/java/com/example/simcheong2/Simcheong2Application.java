47dbc05181bf984e2275878ec95e629ab74ededb
mendel <rhthrhrl0@naver.com>
2024-07-20 16:53:40 +0900

    chore: 프로젝트 추가

---
+++ backend/simcheong2/src/main/java/com/example/simcheong2/Simcheong2Application.java
@@ -0,0 +1,14 @@
+package com.example.simcheong2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class Simcheong2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simcheong2Application.class, args);
+	}
+
+}
---
+++ backend/simcheong2/src/test/java/com/example/simcheong2/Simcheong2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simcheong2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simcheong2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
