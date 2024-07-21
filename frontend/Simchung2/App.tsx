9fb488d57debe5ddcdd2948f37718b2ffc71531d
dmltn <dmltn3426@naver.com>
2024-07-21 22:34:43 +0900

    chore: 다시 세팅

--- frontend/Simchung2/App.tsx
+++ frontend/Simchung2/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>안녕하세요 반갑습니다</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
