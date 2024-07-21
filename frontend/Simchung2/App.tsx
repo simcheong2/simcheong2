79e2b43a7421c09d895a4855915804589dfa3ec7
dmltn <dmltn3426@naver.com>
2024-07-22 02:03:31 +0900

    chore: eslint, prettier 설정

--- frontend/Simchung2/App.tsx
+++ frontend/Simchung2/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'; 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Test from './hooks/Test';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Test/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ frontend/Simchung2/hooks/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Test = () => {
+    return(
+        <View>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    )
+}
+
+export default Test
