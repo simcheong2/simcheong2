b32ecfeefbd6b1dcc13fc3965bde4ee2e7cd1e6e
dmltn <dmltn3426@naver.com>
2024-07-22 02:05:05 +0900

    refactor: 필요없는 Test.tsx 삭제

--- frontend/Simchung2/App.tsx
+++ frontend/Simchung2/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'; 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Test from './hooks/Test';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Test/>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
