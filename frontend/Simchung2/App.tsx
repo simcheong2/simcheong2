db3c7f80b8ae6ddb90d068c32eff8fc2168a4fe7
KimEuiSoo <63793652+KimEuiSoo@users.noreply.github.com>
2024-07-24 08:17:21 +0900

    design: 홈 화면 (#3)

--- frontend/Simchung2/App.tsx
+++ frontend/Simchung2/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomNavigation from './navigation/BottomNavigation';
 import StackNavigation from './navigation/StackNavigation';
