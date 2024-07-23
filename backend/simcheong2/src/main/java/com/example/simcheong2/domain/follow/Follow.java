da6a9ebdfbee3640865f1c91b34dd7143a8d23f7
Todom2 <schoolpang@naver.com>
2024-07-23 20:33:13 +0900

    chore: 패키지 구조 추가 2

---
+++ backend/simcheong2/src/main/java/com/example/simcheong2/domain/follow/Follow.java
@@ -0,0 +1,42 @@
+package com.example.simcheong2.domain.follow;
+
+import com.example.simcheong2.domain.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+public class Follow {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer followId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id", nullable = false)
+    private User following;
+
+    public Follow(Integer followId, User follower, User following) {
+        this.followId = followId;
+        this.follower = follower;
+        this.following = following;
+    }
+}
---
+++ backend/simcheong2/src/main/java/com/example/simcheong2/domain/follow/FollowRepository.java
@@ -0,0 +1,7 @@
+package com.example.simcheong2.domain.follow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FollowRepository extends JpaRepository<Follow, Integer> {
+}
