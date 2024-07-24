90b8ae6baa7e7bb910ad85ee590da3d2b316606e
Todom2 <schoolpang@naver.com>
2024-07-24 16:42:26 +0900

    feat: CommentController 추가

---
+++ backend/simcheong2/src/main/java/com/example/simcheong2/domain/comment/controller/CommentController.java
@@ -0,0 +1,25 @@
+package com.example.simcheong2.domain.comment.controller;
+
+import com.example.simcheong2.domain.comment.controller.reqeust.CreateCommentRequest;
+import com.example.simcheong2.domain.comment.controller.response.CommentResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "댓글 등록 API")
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+public class CommentController {
+    @PostMapping
+    public ResponseEntity<Boolean> createComment(@RequestBody @Valid CreateCommentRequest request){
+        return ResponseEntity.ok(true);
+    }
+}
