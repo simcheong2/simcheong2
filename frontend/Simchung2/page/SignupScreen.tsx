3c364cff33bfc656f82f863ef63c23cb624a7235
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-07-24 16:14:16 +0900

    chore : 회원가입 코드 수정

--- frontend/Simchung2/page/SignupScreen.tsx
+++ frontend/Simchung2/page/SignupScreen.tsx
@@ -1,67 +1,138 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import { TouchableOpacity, StyleSheet, Text, TextInput, View, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 
 function Signupscreen() {
-    return(
+    const [selectedGender, setSelectedGender] = useState("");
+    const [selectedDisability, setSelectedDisability] = useState("");
+
+    return (
         <KeyboardAvoidingView
             style={{ flex: 1 }}
             behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={80} // iOS의 경우 필요한 오프셋 설정
         >
-            <ScrollView contentContainerStyle={styles.container}>
-                <View style={styles.header}>
-                    <Icon name="arrow-left" size={48} />
-                    <Text style={styles.headerText}>회원가입</Text>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <View style={styles.container}>
+                    <View style={styles.header}>
+                        <Icon name="chevron-left" size={48} />
+                        <Text style={styles.headerText}>회원가입</Text>
+                    </View>
+                    <View style={styles.inputSection}>
+                        <View style={[styles.inputWrapper, styles.topInputWrapper]}>
+                            <Icon style={styles.icon} name="person" size={24} />
+                            <TextInput style={styles.textInput} placeholder="아이디" />
+                        </View>
+                        <View style={styles.inputWrapper}>
+                            <Icon style={styles.icon} name="lock" size={24} />
+                            <TextInput style={styles.textInput} placeholder="비밀번호" secureTextEntry={true} />
+                        </View>
+                        <View style={[styles.inputWrapper, styles.bottomInputWrapper]}>
+                            <Icon style={styles.icon} name="email" size={24} />
+                            <TextInput style={styles.textInput} placeholder="이메일" />
+                        </View>
+                    </View>
+                    <View style={styles.inputSection}>
+                        <View style={[styles.inputWrapper, styles.topInputWrapper]}>
+                            <Icon style={styles.icon} name="person" size={24} />
+                            <TextInput style={styles.textInput} placeholder="이름" />
+                        </View>
+                        <View style={styles.inputWrapper}>
+                            <Icon style={styles.icon} name="person" size={24} />
+                            <TextInput style={styles.textInput} placeholder="닉네임" />
+                        </View>
+                        <View style={styles.inputWrapper}>
+                            <Icon style={styles.icon} name="cake" size={24} />
+                            <TextInput style={styles.textInput} placeholder="생년월일 8자리" />
+                        </View>
+                        <View style={styles.choiceTabContainer}>
+                            <Icon style={[styles.icon, styles.tabIcon]} name="wc" size={24} />
+                            <TouchableOpacity
+                                style={[
+                                    styles.choiceTab,
+                                    selectedGender === 'male' && styles.choicedTab,
+                                ]}
+                                onPress={() => setSelectedGender('male')}
+                            >
+                                <Text
+                                    style={[
+                                        styles.choiceTabText,
+                                        selectedGender === 'male' && styles.choicedTabText,
+                                    ]}
+                                >
+                                    남
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={[
+                                    styles.choiceTab,
+                                    selectedGender === 'female' && styles.choicedTab,
+                                ]}
+                                onPress={() => setSelectedGender('female')}
+                            >
+                                <Text
+                                    style={[
+                                        styles.choiceTabText,
+                                        selectedGender === 'female' && styles.choicedTabText,
+                                    ]}
+                                >
+                                    여
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.choiceTabContainer}>
+                            <Icon style={styles.tabIcon} name="accessible" size={24} />
+                            <TouchableOpacity
+                                style={[
+                                    styles.choiceTab,
+                                    selectedDisability === 'disabled' && styles.choicedTab,
+                                ]}
+                                onPress={() => setSelectedDisability('disabled')}
+                            >
+                                <Text
+                                    style={[
+                                        styles.choiceTabText,
+                                        selectedDisability === 'disabled' && styles.choicedTabText,
+                                    ]}
+                                >
+                                    장애인
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={[
+                                    styles.choiceTab,
+                                    selectedDisability === 'nondisabled' && styles.choicedTab,
+                                ]}
+                                onPress={() => setSelectedDisability('nondisabled')}
+                            >
+                                <Text
+                                    style={[
+                                        styles.choiceTabText,
+                                        selectedDisability === 'nondisabled' && styles.choicedTabText,
+                                    ]}
+                                >
+                                    비장애인
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={[styles.inputWrapper, styles.bottomInputWrapper]}>
+                            <Icon style={styles.icon} name="smartphone" size={24} />
+                            <TextInput style={styles.textInput} placeholder="휴대전화번호" />
+                        </View>
+                    </View>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>인증요청</Text>
+                    </TouchableOpacity>
                 </View>
-                <View style={styles.inputSection}>
-                    <View style={[styles.inputWrapper,styles.topInputWrapper]}>
-                        <Icon style={styles.icon} name="person" size={24} />
-                        <TextInput style={styles.textInput} placeholder="아이디" />
-                    </View>
-                    <View style={styles.inputWrapper}>
-                        <Icon style={styles.icon} name="lock" size={24} />
-                        <TextInput style={styles.textInput} placeholder="비밀번호" secureTextEntry={true} />
-                    </View>
-                    <View style={[styles.inputWrapper,styles.bottomInputWrapper]}>
-                        <Icon style={styles.icon} name="email" size={24} />
-                        <TextInput style={styles.textInput} placeholder="이메일" />
-                    </View>
-                </View>
-                <View style={styles.inputSection}>
-                    <View style={[styles.inputWrapper,styles.topInputWrapper]}>
-                        <Icon style={styles.icon} name="person" size={24} />
-                        <TextInput style={styles.textInput} placeholder="이름" />
-                    </View>
-                    <View style={styles.inputWrapper}>
-                        <Icon style={styles.icon} name="cake" size={24} />
-                        <TextInput style={styles.textInput} placeholder="생년월일 8자리" />
-                    </View>
-                    <View style={styles.inputWrapper}>
-                        <Icon style={styles.icon} name="wc" size={24} />
-                        <TextInput style={styles.textInput} placeholder="성별" />
-                    </View>
-                    <View style={styles.inputWrapper}>
-                        <Icon style={styles.icon} name="accessible" size={24} />
-                        <TextInput style={styles.textInput} placeholder="장애인" />
-                    </View>
-                    <View style={styles.inputWrapper}>
-                        <Icon style={styles.icon} name="phone" size={24} />
-                        <TextInput style={styles.textInput} placeholder="통신사 선택" />
-                    </View>
-                    <View style={[styles.inputWrapper,styles.bottomInputWrapper]}>
-                        <Icon style={styles.icon} name="smartphone" size={24} />
-                        <TextInput style={styles.textInput} placeholder="휴대전화번호" />
-                    </View>
-                </View>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>인증요청</Text>
-                </TouchableOpacity>
             </ScrollView>
         </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
+    scrollContainer: {
+        flexGrow: 1,
+    },
     container: {
         flexGrow: 1,
         padding: 16,
@@ -84,17 +155,15 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         backgroundColor: "#F3F4F6",
-      
         padding: 8,
         borderColor: "#555555",
         borderWidth: 1,
-     
     },
-    topInputWrapper : {
+    topInputWrapper: {
         borderTopLeftRadius: 8,
         borderTopRightRadius: 8,
     },
-    bottomInputWrapper : {
+    bottomInputWrapper: {
         borderBottomLeftRadius: 8,
         borderBottomRightRadius: 8,
     },
@@ -110,10 +179,40 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         alignItems: "center",
         marginTop: 16,
+    
     },
     buttonText: {
         color: "#fff",
         fontSize: 16,
+    },
+    choiceTabContainer: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#F3F4F6",
+        borderWidth: 1,
+        borderColor: "#555555",
+    },
+    choiceTab: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: "#F3F4F6",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    tabIcon: {
+        marginLeft: 8,
+    },
+    choiceTabText: {
+        color: "#555555",
+    },
+    choicedTab: {
+        backgroundColor: "#334792",
+        borderColor: "#334792",
+    },
+    choicedTabText: {
+        color: "#fff",
     },
 });
 
