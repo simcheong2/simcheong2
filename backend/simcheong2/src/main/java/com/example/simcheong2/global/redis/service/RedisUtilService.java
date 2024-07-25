bd2a73949a9a5961c3c6c22b86c313a87bea6b6d
Todom2 <schoolpang@naver.com>
2024-07-25 22:07:15 +0900

    feat: Global redis 디렉토리 구조 변경, RedisUtilService 추가

---
+++ backend/simcheong2/src/main/java/com/example/simcheong2/global/redis/service/RedisUtilService.java
@@ -0,0 +1,59 @@
+package com.example.simcheong2.global.redis.service;
+
+import com.example.simcheong2.global.exception.model.CustomException;
+import com.example.simcheong2.global.exception.model.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisUtilService {
+    private final RedisTemplate redisTemplate;
+
+    public String getData(String key){
+        Object result = redisTemplate.opsForValue().get(key);
+        if(result != null){
+            return result.toString();
+        }
+        return null;
+    }
+
+    public void setData(String key, String value){
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        if(!valueOperations.setIfAbsent(key, value, Duration.ofSeconds(3600))){
+            throw new CustomException(ErrorCode.BAD_REQUEST,"Duplication login");
+        }
+    }
+
+    public void setAccessToken(String key, String value){
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        if(!valueOperations.setIfAbsent(key, value, Duration.ofSeconds(43200))){
+            throw new CustomException(ErrorCode.BAD_REQUEST,"Duplication login");
+        }
+    }
+
+    public void setRefreshToken(String key, String value){
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        if(!valueOperations.setIfAbsent(key, value, Duration.ofSeconds(1209600))){
+            throw new CustomException(ErrorCode.BAD_REQUEST,"Duplication login");
+
+        }
+    }
+
+
+    public void setDataExpire(String key, String value, Long duration){
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void deleteData(String key){
+        if(this.getData(key) == null) throw new CustomException(ErrorCode.BAD_REQUEST,"No data in Redis");
+        redisTemplate.delete(key);
+    }
+
+}
