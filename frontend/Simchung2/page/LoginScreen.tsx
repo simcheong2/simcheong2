8c703ecf9e907e52193480f4f231b1cc7cf80e83
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-07-26 17:37:03 +0900

    feat : 로그인 화면 수정

--- frontend/Simchung2/page/LoginScreen.tsx
+++ frontend/Simchung2/page/LoginScreen.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 function LoginScreen() {
     return(
        <View style={styles. container}>
-        
+        <Image source={require('../assets/images/logo.png')}/>
+        <TextInput placeholder="아이디"/>
+        <View>
+            <TextInput placeholder="비밀번호"/>
+            <Icon name="eye-off-outline"/>
+        </View>
+        <View>
+
+            <Text>아이디 저장</Text>
+        </View>
+        <TouchableOpacity>
+            <Text>로그인</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text>회원가입</Text>
+        </TouchableOpacity>
        </View>
     )
 }
