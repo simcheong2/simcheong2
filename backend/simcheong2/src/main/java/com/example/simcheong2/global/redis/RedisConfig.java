de5406708066403c5513e13525ddb8a9591ed3e0
Todom2 <schoolpang@naver.com>
2024-07-28 19:40:06 +0900

    refactor: RedisConfig 경로수정.

--- backend/simcheong2/src/main/java/com/example/simcheong2/global/redis/RedisConfig.java
+++ backend/simcheong2/src/main/java/com/example/simcheong2/global/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.simcheong2.global.redis.config;
+package com.example.simcheong2.global.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
