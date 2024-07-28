aa1a07144b1637e59b4cde7ad071257b563acac1
mendel <rhthrhrl0@naver.com>
2024-07-28 20:13:00 +0900

    feat: 토큰 유효시간 properties로 분리

--- backend/simcheong2/src/main/java/com/example/simcheong2/global/redis/service/RedisUtilService.java
+++ backend/simcheong2/src/main/java/com/example/simcheong2/global/redis/service/RedisUtilService.java
@@ -4,6 +4,7 @@ import com.example.simcheong2.global.exception.model.CustomException;
 import com.example.simcheong2.global.exception.model.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -17,48 +18,32 @@ import java.util.Objects;
 public class RedisUtilService {
     private final RedisTemplate redisTemplate;
 
-    public String getData(String key){
+    @Value("${jwt.token.time}")
+    private long ACCESS_TOKEN_EXPIRE_TIME;
+
+    @Value("${jwt.token.refresh-time}")
+    private long REFRESH_TOKEN_EXPIRE_TIME;
+
+    public String getData(String key) {
         Object result = redisTemplate.opsForValue().get(key);
-        if(result != null){
+        if (result != null) {
             return result.toString();
         }
         return null;
     }
 
-    public void setData(String key, String value){
+    public void setAccessToken(String key, String value) {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        if(!valueOperations.setIfAbsent(key, value, Duration.ofSeconds(3600))){
-            throw new CustomException(ErrorCode.BAD_REQUEST,"Duplication login");
-        }
+        valueOperations.set(key, value, Duration.ofSeconds(ACCESS_TOKEN_EXPIRE_TIME));
     }
 
-    public void setAccessToken(String key, String value){
+    public void setRefreshToken(String key, String value) {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(key,value,Duration.ofSeconds(60));
-        /*if(!valueOperations.setIfAbsent(key, value, Duration.ofSeconds(43200))){
-            throw new CustomException(ErrorCode.BAD_REQUEST,"Duplication login");
-        }
-         */
+        valueOperations.set(key, value, Duration.ofSeconds(REFRESH_TOKEN_EXPIRE_TIME));
     }
 
-    public void setRefreshToken(String key, String value){
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(key,value,Duration.ofSeconds(1209600));
-        /*if(!valueOperations.setIfAbsent(key, value, Duration.ofSeconds(1209600))){
-            throw new CustomException(ErrorCode.BAD_REQUEST,"Duplication login");
-        }
-         */
-    }
-
-
-    public void setDataExpire(String key, String value, Long duration){
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        Duration expireDuration = Duration.ofSeconds(duration);
-        valueOperations.set(key, value, expireDuration);
-    }
-
-    public void deleteData(String key){
-        if(this.getData(key) == null) throw new CustomException(ErrorCode.BAD_REQUEST,"No data in Redis");
+    public void deleteData(String key) {
+        if (this.getData(key) == null) throw new CustomException(ErrorCode.BAD_REQUEST, "No data in Redis");
         redisTemplate.delete(key);
     }
 
--- backend/simcheong2/src/main/java/com/example/simcheong2/global/service/TokensGenerateService.java
+++ backend/simcheong2/src/main/java/com/example/simcheong2/global/service/TokensGenerateService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -14,14 +15,18 @@ import java.util.UUID;
 @Component
 @RequiredArgsConstructor
 public class TokensGenerateService {
-    private static final String BEARER_TYPE ="Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 60;
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1209600;
+    private static final String BEARER_TYPE = "Bearer";
+
+    @Value("${jwt.token.time}")
+    private long ACCESS_TOKEN_EXPIRE_TIME;
+
+    @Value("${jwt.token.refresh-time}")
+    private long REFRESH_TOKEN_EXPIRE_TIME;
     private static final String ACCESS_TOKEN_KIND = "accessToken";
     private static final String REFRESH_TOKEN_KIND = "refreshToken";
     private final JwtTokenService jwtTokenService;
 
-    public Tokens generate(Integer memberID, String inputId){
+    public Tokens generate(Integer memberID, String inputId) {
         long now = (new Date()).getTime();
         Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME * 1000);
         Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME * 1000);
@@ -30,8 +35,9 @@ public class TokensGenerateService {
         String accessToken = jwtTokenService.generate(inputId, ACCESS_TOKEN_KIND, accessTokenExpiredAt);
         String refreshToken = jwtTokenService.generate(id, REFRESH_TOKEN_KIND, refreshTokenExpiredAt);
 
-        return Tokens.of(accessToken,refreshToken,BEARER_TYPE,ACCESS_TOKEN_EXPIRE_TIME);
+        return Tokens.of(accessToken, refreshToken, BEARER_TYPE, ACCESS_TOKEN_EXPIRE_TIME);
     }
+
     public String extractMemberId(String accessToken) {
         return String.valueOf(jwtTokenService.extractSubject(accessToken));
     }
