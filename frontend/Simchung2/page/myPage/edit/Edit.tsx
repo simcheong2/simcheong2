969a71180f676601962b1a0778e287bb0e6f9d0a
dmltn <dmltn3426@naver.com>
2024-07-29 20:51:31 +0900

    design: 내 갤러리 적용

--- frontend/Simchung2/page/myPage/edit/Edit.tsx
+++ frontend/Simchung2/page/myPage/edit/Edit.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { MyProfile } from '../../../interface/user/Profile';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useNavigation } from '@react-navigation/native';
+import { FeedNavigationProp } from '../../../types/navigationTypes';
+import TextFields from './TextFields';
+import ProfileEdit from './ProfileEdit';
 
 interface EditProps{
     profile: MyProfile
 }
 
 const Edit = ({profile}:EditProps) => {
+    const navigation = useNavigation<FeedNavigationProp>();
+    const [name, setName] = useState<string>('');
+
+    const setNameHandler = (e: string) => {
+        setName((name) => (name=e));
+    }
+
     return(
         <View style={styles.container}>
-            <Text>텍스트</Text>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Icon name="chevron-left" size={48} />
+                </TouchableOpacity>
+                <View style={styles['header-container']}>
+                    <Text style={styles['header-title']}>프로필 편집</Text>
+                </View>
+            </View>
+            <ProfileEdit profile={profile}/>
         </View>
     )
 }
@@ -20,7 +40,26 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
         backgroundColor: "#fff",
-    }
+    },
+
+    header: {
+        width: '100%',
+        padding: 12,
+        marginTop: 18,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    'header-container': {
+        width:"72%",
+        alignItems: 'center',
+    },
+
+    'header-title': {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#555',
+    },
 })
 
 export default Edit
