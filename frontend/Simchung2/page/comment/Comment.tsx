155b41763f2cc0acd0ce2a236bca8d64dbf2a689
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-02 02:53:19 +0900

    chore : 코드 수정

--- frontend/Simchung2/page/comment/Comment.tsx
+++ frontend/Simchung2/page/comment/Comment.tsx
@@ -1,17 +1,47 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { Comments } from '../../interface/feed/Feed';
 
-interface CommentProps{
+interface CommentProps {
     comment: Comments;
 }
 
-const Comment = ({comment}: CommentProps) => {
-    return(
-        <View>
-            <Text>{comment.comment}</Text>
+const Comment = ({ comment }: CommentProps) => {
+    return (
+        <View style={styles.commentContainer}>
+            <View style={styles.header}>
+                <Text style={styles.username}>{comment.nickname}</Text>
+
+            </View>
+            <Text style={styles.commentText}>{comment.comment}</Text>
         </View>
     )
 }
 
-export default Comment
+const styles = StyleSheet.create({
+    commentContainer: {
+        backgroundColor: 'rgba(0, 0, 0, 0.05)', // 배경을 약간 투명하게 설정
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 8,
+        borderColor: '#ddd',
+        borderWidth: 1,
+        marginLeft:10,
+        marginRight:10,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 5,
+    },
+    username: {
+        fontWeight: 'bold',
+        color: '#333',
+    },
+
+    commentText: {
+        color: '#333',
+    }
+})
+
+export default Comment;
