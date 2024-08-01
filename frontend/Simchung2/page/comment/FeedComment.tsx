66617c8ab9d6965c367ab5be4f4032a58cab11b9
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-08-02 03:20:14 +0900

    Feature/fe comments (#40)
    * chore : comments 첫 커밋
    
    * Feature/1 voice (#37)
    
    * feature: 좋아요
    
    * feature: 사진 업로드 음성 로드
    
    * feature: postAtom 추가 (#38)
    
    * chore : 코드 수정
    
    * chore : 커스텀시트 뒷배경 수정
    
    * design: 업로드 수정 (#39)
    
    ---------
    
    Co-authored-by: KimEuiSoo <63793652+KimEuiSoo@users.noreply.github.com>

--- frontend/Simchung2/page/comment/FeedComment.tsx
+++ frontend/Simchung2/page/comment/FeedComment.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -9,14 +9,149 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
 import { useRecoilValue } from "recoil";
-import commentsAtom from "../../recoil/atom/commentsAtom";
-import { Comments } from "../../interface/feed/Feed";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { Audio } from 'expo-av';
+import axios from 'axios';
+import accessTokenAtom from "../../recoil/atom/accessTokenAtom";
 import Comment from './Comment';
+import Loading from "../loading/Loading";
+import postAtom from "../../recoil/atom/postAtom";
 
 const FeedComment = () => {
-  const comments = useRecoilValue<Comments[]>(commentsAtom);
+  const postId = useRecoilValue(postAtom);
+  const [mic, setMic] = useState(false);
+  const [recording, setRecording] = useState<Audio.Recording | undefined>(undefined);
+  const [text, setText] = useState<string>('');
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+  const accessToken = useRecoilValue(accessTokenAtom);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [comments, setComments] = useState<any[]>([]); // State to store comments
+
+  const formCommentData={
+    postId: postId,
+    content: '',
+  };
+
+  const baseURL = 'http://www.my-first-develop-library.shop:8080';
+
+  useEffect(() => {
+    handleGetPostData();
+  }, []);
+
+  const handleGetPostData = () => {
+    axios
+        .get(`${baseURL}/posts/main`,{
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          }
+        })
+        .then((response) => {
+          if (response.status == 200) {
+
+              const postData = response.data;
+
+              const post = postData.find((post:any)=>post.posts.postId===postId)
+              if (post) {
+                setComments(post.comments);
+              }
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching post ID', error);
+        });
+  };
+  const handleAddComment = () =>{
+      axios
+          .post(`${baseURL}/comments`, formCommentData,{headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "Content-Type": 'application/json',
+            }})
+          .then((response)=>{
+              if(response.status===200){
+                  console.log("댓글달기 성공!");
+                  handleGetPostData();
+              }
+          })
+          .catch((error)=>{
+            console.error(error);
+          })
+          .finally(()=>{
+            console.log(formCommentData);
+          })
+  }
+  const onTextInputFocus = () => {
+    setMic(true);
+  }
+
+  const onTextChange = (e: string) => {
+    setText(e);
+  }
+
+  async function startRecording() {
+    try {
+      if (permissionResponse?.status !== 'granted') {
+        console.log('Requesting permission..');
+        await requestPermission();
+      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording?.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording?.getURI();
+    if (uri) {
+      upload(uri);
+    }
+    console.log('Recording stopped and stored at', uri);
+  }
+
+  const upload = (uri: string) => {
+    const formData = new FormData();
+    const fileName = uri.split('/').pop();
+
+    formData.append('audio', {
+      uri: uri,
+      name: fileName,
+      type: 'audio/mp3',
+    });
+
+    setLoading(true);
+    axios.post(`${baseURL}/users/audio`, formData, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    }).then((response) => {
+      console.log('Success', response.data);
+      formCommentData.content = response.data
+      setText(response.data);
+      handleAddComment();
+      setText("");
+    }).catch((error) => {
+      console.log('Fail', error);
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
 
   return (
       <KeyboardAvoidingView
@@ -31,17 +166,36 @@ const FeedComment = () => {
             </View>
             <SafeAreaView style={styles["comments-container"]}>
               <ScrollView style={styles["comments-scroll"]}>
-                {comments.map((comment, index)=>(
-                    <Comment key={index} comment={comment}/>
-                ))}
+                {comments.length > 0 ? (
+                    comments.map((comment, index) => (
+                        <Comment key={index} comment={comment} />
+                    ))
+                ) : (
+                    <Text style={styles.noCommentsText}>아직 댓글이 없습니다.</Text>
+                )}
               </ScrollView>
             </SafeAreaView>
             <View style={styles["comment-register"]}>
               <TextInput
                   style={styles.comment}
                   placeholder="댓글"
+                  onFocus={onTextInputFocus}
+                  onChangeText={onTextChange}
+                  value={text}
               />
             </View>
+            {mic && (
+                <View style={styles["mic-wrapper"]}>
+                  <TouchableOpacity
+                      style={styles["mic-container"]}
+                      onPress={recording ? stopRecording : startRecording}
+                  >
+                    <Icon name={"mic"} size={48} color={recording ? "#ff6347" : "#ffffff"} />
+                  </TouchableOpacity>
+                  {recording && <Text style={styles.recordingText}>Recording...</Text>}
+                </View>
+            )}
+            {loading && <Loading />}
           </View>
         </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
@@ -51,6 +205,7 @@ const FeedComment = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
   },
   inner: {
     flex: 1,
@@ -71,6 +226,7 @@ const styles = StyleSheet.create({
   },
   "comments-scroll": {
     width: "100%",
+    backgroundColor: "#FFFFFF"
   },
   "comment-register": {
     width: "100%",
@@ -84,6 +240,31 @@ const styles = StyleSheet.create({
     borderColor: "#555555",
     borderWidth: 1,
     paddingHorizontal: 12,
+    padding: 6,
+    borderRadius: 8,
+  },
+  "mic-wrapper": {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  "mic-container": {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: "#ff6347",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 2,
+  },
+  recordingText: {
+    marginTop: 10,
+    color: "#ff6347",
+    fontWeight: "bold",
+  },
+  noCommentsText: {
+    textAlign: "center",
+    color: "#fff",
+    marginTop: 20,
   },
 });
 
